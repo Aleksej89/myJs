ec33ff1f4c9ef6835ca315b1ad6ec58bc746c8ad
Aleksej89 <aleksey.zhuravlev1989@gmail.com>
2022-01-13 16:10:45 +0200

    test

--- homeWork01/exercise_6.js
+++ homeWork01/exercise_6.js
@@ -1,1 +1,13 @@
 "use strict";
+
+let arr = [1, 2, 3, 4];
+
+function arraySum(arr) {
+    let sum = 0;
+    for (var i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+console.log(arraySum(arr));
