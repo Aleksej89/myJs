440a87ea4014f34a8d7c8f9e573bd00f6485d5a2
Aleksej89 <aleksey.zhuravlev1989@gmail.com>
2022-01-13 17:00:05 +0200

    test

--- homeWork01/exercise_13.js
+++ homeWork01/exercise_13.js
@@ -1,1 +1,14 @@
 "use strict";
+
+let arr = [1, 2, 5, 9, 4, 13, 4, 10];
+
+function arraySum(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] === 4) {
+            console.log("Есть!");
+            break;
+        }
+    }
+}
+
+arraySum(arr);
