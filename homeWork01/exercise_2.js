14c17456fe4c96fc00c8eb5e3ada8864ad0c03d5
Aleksej89 <aleksey.zhuravlev1989@gmail.com>
2022-01-13 10:37:34 +0200

    test

--- homeWork01/exercise_2.js
+++ homeWork01/exercise_2.js
@@ -1,1 +1,10 @@
 "use strict";
+
+function checkLogin(login){
+    return (login == "Pitter") ? "Hi" :
+    (login == "Owner") ? "Hello" :
+    (login == "") ? "unknown" :
+    "default";
+}
+    
+console.log(checkLogin("sdasd"));
