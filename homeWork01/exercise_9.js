7114725eb23a69f66f3d07da81ccf6b9cfa6acb4
Aleksej89 <aleksey.zhuravlev1989@gmail.com>
2022-01-13 16:28:05 +0200

    est

--- homeWork01/exercise_9.js
+++ homeWork01/exercise_9.js
@@ -1,1 +1,21 @@
 "use strict";
+
+const bubbleSort = (coll) => {
+    let stepsCount = coll.length - 1;
+    let swapped;
+    do {
+        swapped = false;
+        for (let i = 0; i < stepsCount; i += 1) {
+            if (coll[i] < coll[i + 1]) {
+                const temp = coll[i];
+                coll[i] = coll[i + 1];
+                coll[i + 1] = temp;
+                swapped = true;
+            }
+        }
+        stepsCount -= 1;
+    } while (swapped);
+    return coll;
+};
+
+console.log(bubbleSort([1,2,3,4,5,6]));
