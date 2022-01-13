c9eb50ff00699e093ff45d1fe348e6ef4db1a78d
Aleksej89 <aleksey.zhuravlev1989@gmail.com>
2022-01-13 17:09:21 +0200

    test

--- homeWork01/exercise_14.js
+++ homeWork01/exercise_14.js
@@ -1,1 +1,20 @@
 "use strict";
+
+function division(number) {
+    let countDivisionOperations = 0;
+    do {
+        number = number / 2;
+        countDivisionOperations++;
+    } while (number > 50);
+
+    let answer = {
+        number: number,
+        count: countDivisionOperations
+    };
+    return answer;
+}
+
+let myAnswer = division(1000);
+
+console.log(myAnswer.number);
+console.log(myAnswer.count);
