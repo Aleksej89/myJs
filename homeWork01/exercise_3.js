170c1e3dc9c1afc471a2a2c7b706bd580d0842fb
Aleksej89 <aleksey.zhuravlev1989@gmail.com>
2022-01-13 15:57:43 +0200

    test

--- homeWork01/exercise_3.js
+++ homeWork01/exercise_3.js
@@ -1,1 +1,13 @@
 "use strict";
+
+function ifElse(key) {
+    if (key === "a") {
+        console.log("a");
+    } else if (key === "b" || key === "c" || key === "c" || key === "e") {
+        console.log("others");
+    } else {
+        console.log("unknown");
+    }
+}
+
+ifElse("c");
