8f81ae8f41cfb0ce238ab8aa907b8775c5b9964f
Aleksej89 <aleksey.zhuravlev1989@gmail.com>
2022-01-13 16:04:01 +0200

    test

--- homeWork01/exercise_4.js
+++ homeWork01/exercise_4.js
@@ -1,1 +1,20 @@
 "use strict";
+
+function mySwitch(key) {
+    switch (key) {
+        case 0:
+            console.log("0");
+            break;
+        case 1:
+            console.log("1");
+            break;
+        case 2:
+        case 3:
+            console.log("2,3");
+            break;
+        default:
+            console.log("default");
+    }
+}
+
+mySwitch(2);
