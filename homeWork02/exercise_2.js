aa2a47d7d291d6b54fea4c27ed2f76ce762cd090
Aleksej89 <aleksey.zhuravlev1989@gmail.com>
2022-01-19 13:56:08 +0200

    test

--- homeWork02/exercise_2.js
+++ homeWork02/exercise_2.js
@@ -1,30 +1,25 @@
 "use strict";
 
-function funk(...args) {
-    if (isArgsNorm(args)) {
-        let sum = 0;
-        for (let i = 0; i < args.length; i++) {
-            sum += args[i];
-        }
-        return sum;
+function funk(a, b, c) {
+    if (isArgsNorm(a, b, c)) {
+        return (a - b) / c;
     } else {
         throw new Error("all parameters type should be a Number");
     }
 };
 
-function isArgsNorm(value) {
-    for (let i = 0; i < value.length; i++) {
-        if (!isAN(value[i])) {
-            return false;
-        }
+function isArgsNorm(a, b, c) {
+    if (isAN(a) && isAN(b) && isAN(c)) {
+        return true;
     }
-    return true;
+    return false;
 };
 
 function isAN(value) {
     if (value instanceof Number)
         value = value.valueOf();
     return isFinite(value) && value === parseInt(value, 10);
-}
-console.log(funk(1, 2, 3, 4));
-console.log(funk(1, 2, 's', 4));
+};
+
+console.log(funk(1, 2, 3));
+console.log(funk(1, 2, 's'));
