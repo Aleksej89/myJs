c79cd512807af8e2e198e2ccfd1a36949db6c7f0
Aleksej89 <aleksey.zhuravlev1989@gmail.com>
2022-01-19 18:27:10 +0200

    test

--- homeWork02/exercise_6.js
+++ homeWork02/exercise_6.js
@@ -1,2 +1,19 @@
 "use strict";
 
+function isEven(value) {
+    if (isAN(value)) {
+        return value % 2 === 0;
+    } else {
+        throw new Error("parameter type is not a Number");
+    }
+}
+
+function isAN(value) {
+    if (value instanceof Number)
+        value = value.valueOf();
+    return isFinite(value) && value === parseInt(value, 10);
+};
+
+console.log(isEven(4));
+console.log(isEven(3));
+console.log(isEven("asdasd"));
