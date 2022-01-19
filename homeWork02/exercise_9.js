202b189adcf1b072f86c4d9e12cd7f6c95a6de46
Aleksej89 <aleksey.zhuravlev1989@gmail.com>
2022-01-19 13:25:53 +0200

    test

--- homeWork02/exercise_9.js
+++ homeWork02/exercise_9.js
@@ -1,68 +1,74 @@
 "use strict";
 
-const DEFAULT_NUMB = 0;
+const DEFAULT_VALUE = 0;
 
 function addition(arrFirst, arrSecond) {
     if (Array.isArray(arrFirst) && Array.isArray(arrSecond)) {
-        if (arrFirst.length === DEFAULT_NUMB) {
+        if (arrFirst.length === DEFAULT_VALUE) {
             return arrSecond;
-        } else if (arrSecond.length === DEFAULT_NUMB) {
+        } else if (arrSecond.length === DEFAULT_VALUE) {
             return arrFirst;
-        } else if (arrFirst.length === DEFAULT_NUMB && arrSecond.length === DEFAULT_NUMB) {
+        } else if (arrFirst.length === DEFAULT_VALUE && arrSecond.length === DEFAULT_VALUE) {
             return [];
         } else {
-            return getAnswer(getNumberFromArr(arrFirst), getNumberFromArr(arrSecond));
+            return getAnswer(arrFirst, arrSecond);
         }
     } else {
         throw new Error("One of the arguments is not Array");
     }
 };
 
-function getNumberFromArr(arr) {
+function getAnswer(arrFirst, arrSecond) {
+    return getAnswerArr(getNumberStrFromArr(arrFirst), getNumberStrFromArr(arrSecond));
+};
+
+function getNumberStrFromArr(arr) {
     let number = "";
-    if (arr.length !== DEFAULT_NUMB) {
-        for (let i = 0; i < arr.length; i++) {
+    if (arr.length !== DEFAULT_VALUE) {
+        for (let i = DEFAULT_VALUE; i < arr.length; i++) {
             if (typeof arr[i] === "number") {
                 number += arr[i];
             }
         }
     }
-    return BigInt(number);
+    return number;
 };
 
-function getAnswer(nfirstNum, nsecondNum) {
-    if (!nfirstNum) {
-        nfirstNum = DEFAULT_NUMB;
+function getAnswerArr(numeratorStr, denominatorStr) {
+    let numerator = BigInt(numeratorStr);
+    let denominator = BigInt(denominatorStr);
+    if (!numerator) {
+        numerator = DEFAULT_VALUE;
     }
-    if (!nsecondNum) {
-        nsecondNum = DEFAULT_NUMB;
+    if (!denominator) {
+        denominator = DEFAULT_VALUE;
     }
-    let temp = DEFAULT_NUMB;
-    if (nsecondNum > nfirstNum) {
-        temp = nsecondNum;
-        nsecondNum = nfirstNum;
-        nfirstNum = temp;
+    let tempValue = DEFAULT_VALUE;
+    if (denominator > numerator) {
+        tempValue = denominator;
+        denominator = numerator;
+        numerator = tempValue;
     }
-    let sfirstNum = String(nfirstNum);
-    let ssecondNum = String(nsecondNum);
+    let strNumerator = String(numerator);
+    let strDenominator = String(denominator);
     let units = "";
-    if (sfirstNum.length > ssecondNum.length) {
-        for (let i = 0; i < ssecondNum.length; i++) {
-            if ((BigInt(sfirstNum[i]) + BigInt(ssecondNum[i])) > 9) units += 1;
+    if (strNumerator.length > strDenominator.length) {
+        for (let i = 0; i < strDenominator.length; i++) {
+            if ((BigInt(strNumerator[i]) + BigInt(strDenominator[i])) > 9) units += 1;
             else units += " ";
         }
     } else {
-        for (let i = 0; i < sfirstNum.length; i++) {
-            if ((BigInt(sfirstNum[i]) + BigInt(ssecondNum[i])) > 9) units += 1;
+        for (let i = 0; i < strNumerator.length; i++) {
+            if ((BigInt(strNumerator[i]) + BigInt(strDenominator[i])) > 9) units += 1;
             else units += " ";
         }
     }
-    let answer = String(nfirstNum + nsecondNum).split("");
-    for (let i = DEFAULT_NUMB; i < answer.length; i++) {
+    let answer = String(numerator + denominator).split("");
+    for (let i = DEFAULT_VALUE; i < answer.length; i++) {
         answer[i] = Number(answer[i]);
     }
     return answer;
-};
+}
 
 console.log(addition([2, 4, 3], [5, 6, 4]));
 console.log(addition([1, 4, 5], [4, 4, 2]));
