e6cdc120317fcc1f840015828cff5b0274134953
Aleksej89 <aleksey.zhuravlev1989@gmail.com>
2022-01-19 18:47:51 +0200

    TEST

--- homeWork02/exercise_8.js
+++ homeWork02/exercise_8.js
@@ -1,1 +1,23 @@
 "use strict";
+
+
+function consoleRecMain(arr) {
+    if (!Array.isArray(arr)) {
+        throw new Error("parameter type should be an array");
+    } else {
+        if (arr.length === 0) {
+            throw new Error("parameter can't be an empty");
+        } else {
+            consoleRec(arr, 0);
+        }
+    }
+}
+
+function consoleRec(arr, n) {
+    console.log(arr[n++]);
+    if (n < arr.length) consoleRec(arr, n);
+};
+
+consoleRecMain(["AASD","ASDASD","333"]);
+consoleRecMain([1,2,3]);
+consoleRecMain("ASDASD");
