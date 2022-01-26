a9b5ffed64954d46a06a26c055846889e48900a8
Aleksej89 <aleksey.zhuravlev1989@gmail.com>
2022-01-26 14:04:16 +0200

    test

---
+++ homeWork03/exercise_3.js
@@ -0,0 +1,26 @@
+"use strict";
+
+function every(arr, filterFunc) {
+    if (Array.isArray(arr) && isFunction(filterFunc)) {
+        for (let i = 0; i < arr.length; i++) {
+            if (!filterFunc(arr[i])) return false;
+        }
+        return true;
+    } else {
+        throw new Error("Check your parameters!");
+    }
+}
+
+function isFunction(param) {
+    return typeof param === "function";
+}
+
+function isBigEnough(element, index, array) {
+    return element >= 10;
+}
+
+let numbers = [3, 4, 5, 7, 8, 11, 13, 15, 17, 19, 21, 23, 27, 29, 31, 33, 37, 39, 41];
+
+let primes = every(numbers, isBigEnough);
+
+console.log(primes);
