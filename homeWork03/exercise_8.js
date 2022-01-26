7406fd7f321019b91ce897a065812b3390e01f00
Aleksej89 <aleksey.zhuravlev1989@gmail.com>
2022-01-26 09:27:11 +0200

    test

---
+++ homeWork03/exercise_8.js
@@ -0,0 +1,42 @@
+"use strict";
+
+function summArr(arr) {
+    let sum = 0;
+    if (Array.isArray(arr)) {
+        if (arr.length === 0) {
+            sum = 0;
+        } else {
+            for (let i = 0; i < arr.length; i++) {
+                if (!Array.isArray(arr[i])) {
+                    if (isAN(arr[i])) {
+                        sum += arr[i];
+                    } else {
+                        throw new Error("it is not a number!");
+                    }
+                } else {
+                    sum += summArr(arr[i]);
+                }
+            }
+        }
+        return sum;
+    } else {
+        throw new Error("it is not Array!");
+    }
+}
+
+function isAN(value) {
+    if (value instanceof Number)
+        value = value.valueOf();
+    return isFinite(value) && value === parseInt(value, 10);
+};
+
+const arr = [[[1, 2], [1, 2]], [[2, 1], [1, 2]]];
+console.log(summArr(arr)); // 12
+const arr2 = [[[[[1, 2]]]]];
+console.log(summArr(arr2)); // 3
+const arr3 = [[[[[1, 2]]], 2], 1];
+console.log(summArr(arr3)); // 6
+const arr4 = [[[[[]]]]];
+console.log(summArr(arr4)); // 0
+const arr5 = [[[[[], 3]]]];
+console.log(summArr(arr5)); // 3
