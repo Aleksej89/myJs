888969154c837adbae217c2d016c06e79a758961
Aleksej89 <aleksey.zhuravlev1989@gmail.com>
2022-01-26 14:29:49 +0200

    test

---
+++ homeWork03/exercise_5.js
@@ -0,0 +1,42 @@
+"use strict";
+
+function reduce(arr, filterFunc, acc) {
+    if (Array.isArray(arr) && isFunction(filterFunc) && isNormParam(acc)) {
+        for (let i = 0; i < arr.length; i++) {
+            acc = filterFunc(arr[i], acc);
+        }
+        return acc;
+    } else {
+        throw new Error("Check your parameters!");
+    }
+}
+
+function addition(add, result) {
+    return result + add;
+}
+
+function isFunction(param) {
+    return typeof param === "function";
+}
+
+function isNormParam(param) {
+    if (isAN(param) || isString(param)) return true;
+    return false;
+}
+
+function isAN(value) {
+    if (value instanceof Number)
+        value = value.valueOf();
+    return isFinite(value) && value === parseInt(value, 10);
+}
+
+function isString(value) {
+    return typeof value === "string";
+}
+
+let arr = [1, 2, 3];
+let acc = "0";
+
+acc = reduce(arr, addition, acc);
+
+console.log(acc);
