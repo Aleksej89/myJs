b61636894bbe9d0b5f5b9543ce60e1b947b88059
Aleksej89 <aleksey.zhuravlev1989@gmail.com>
2022-01-27 14:48:11 +0200

    test

--- homeWork03/exercise_2.js
+++ homeWork03/exercise_2.js
@@ -1,10 +1,11 @@
 "use strict";
 
+
 function filter(arr, filterFunc) {
     if (Array.isArray(arr) && isFunction(filterFunc)) {
         let newArr = [];
         for (let i = 0; i < arr.length; i++) {
-            if (filterFunc(arr[i])) newArr.push(arr[i]);
+            if (filterFunc(arr[i], i, arr)) newArr.push(arr[i]);
         }
         return newArr;
     } else {
